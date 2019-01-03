4579cf940650b2977b0f7635e5bf12dec62e2d04
Fabien Chouteau <fabien.chouteau@gmail.com>
2019-01-03 19:32:53 +0100

    LV.Objx.Arc: Copying comments from the C headers

--- src/lv-objx-arc.ads
+++ src/lv-objx-arc.ads
@@ -8,27 +8,62 @@ package Lv.Objx.Arc is
 
    subtype Style_T is Uint8_T;
 
+   --  Create a arc objects
+   --  @param par pointer to an object, it will be the parent of the new arc
+   --  @param copy pointer to a arc object, if not NULL then the new object will be copied from it
+   --  @return pointer to the created arc
    function Create (Parent : Obj_T; Copy : Instance) return Instance;
-   pragma Import (C, Create, "lv_arc_create");
 
-   procedure Set_Angles (Self : Instance; Arg2 : Uint16_T; Arg3 : Uint16_T);
-   pragma Import (C, Set_Angles, "lv_arc_set_angles");
+   ----------------------
+   -- Setter functions --
+   ----------------------
 
+   --  Set the start and end angles of an arc. 0 deg: bottom, 90 deg: right etc.
+   --  @param arc pointer to an arc object
+   --  @param start the start angle [0..360]
+   --  @param end the end angle [0..360]
+   procedure Set_Angles (Self : Instance; Start : Uint16_T; End_P : Uint16_T);
+
+   -- Set a style of a arc.
+   -- @param arc pointer to arc object
+   -- @param type which style should be set
+   -- @param style pointer to a style
    procedure Set_Style
-     (Self : Instance;
-      Arg2 : Style_T;
-      Arg3 : access Lv.Style.Style);
-   pragma Import (C, Set_Style, "lv_arc_set_style");
+     (Self   : Instance;
+      Type_P : Style_T;
+      Style  : access Lv.Style.Style);
 
+   ----------------------
+   -- Getter functions --
+   ----------------------
+
+   -- Get the start angle of an arc.
+   -- @param arc pointer to an arc object
+   -- @return the start angle [0..360]
    function Get_Angle_Start (Self : Instance) return Uint16_T;
-   pragma Import (C, Get_Angle_Start, "lv_arc_get_angle_start");
 
+   --  Get the end angle of an arc.
+   --  @param arc pointer to an arc object
+   --  @return the end angle [0..360]
    function Get_Angle_End (Self : Instance) return Uint16_T;
-   pragma Import (C, Get_Angle_End, "lv_arc_get_angle_end");
 
+   -- Get style of a arc.
+   -- @param arc pointer to arc object
+   -- @param type which style should be get
+   -- @return style pointer to the style
    function Get_Style
      (Self : Instance;
       Arg2 : Style_T) return access Lv.Style.Style;
-   pragma Import (C, Get_Style, "lv_arc_get_style");
+
+   -------------
+   -- Imports --
+   -------------
+
+   pragma Import (C, Create,          "lv_arc_create");
+   pragma Import (C, Set_Angles,      "lv_arc_set_angles");
+   pragma Import (C, Set_Style,       "lv_arc_set_style");
+   pragma Import (C, Get_Angle_Start, "lv_arc_get_angle_start");
+   pragma Import (C, Get_Angle_End,   "lv_arc_get_angle_end");
+   pragma Import (C, Get_Style,       "lv_arc_get_style");
 
 end Lv.Objx.Arc;
